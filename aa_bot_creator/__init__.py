5837d9a8a4a97b66fb6b3a6655ce1045c4d621fb
SrishitaReddy <108647934+SrishitaReddy@users.noreply.github.com>
2025-03-13 21:43:48 +0530

    Add aa_bot_creator/__init__.py

---
+++ aa_bot_creator/__init__.py
@@ -0,0 +1,8 @@
+"""
+AA Bot Creator - A tool for automating the creation of Automation Anywhere bots from BRDs.
+
+This package provides functionality for analyzing BRDs, generating documentation,
+and building Automation Anywhere bots.
+"""
+
+__version__ = '0.1.0'
